3b84f10b9a45d19aa6b3dc6fde929277759b71a2
rahulbarik23 <89737781+rahulbarik23@users.noreply.github.com>
2023-01-07 23:04:54 +0530

    initial commit

--- script.js
+++ script.js
@@ -8,20 +8,56 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  let arraydevelopers = arr.map(function(element)
+  {
+      if(element.profession === "developer")
+      {
+        console.log(element)
+      }
+  })
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  let arraydev = arr.forEach(function(array)
+  {
+      if(array.profession === "developer")
+      {
+        console.log(array)
+      }
+  })
 }
 
 function addData() {
   //Write your code here
+  arr.push({id:4,name:"susan",age:"20",profession:"intern"})
+  console.log(arr)
 }
 
 function removeAdmin() {
   //Write your code here
+  let updatArray = arr.filter(function(array)
+  {
+    if(array.profession !== "admin")
+    {
+      return array
+    }
+  })
+
+  console.log(updatArray);
 }
 
 function concatenateArray() {
   //Write your code here
+  let userarray = [
+      {id: 9, name: "karan", age: "19", profession: "worker"},
+      {id: 24, name: "ram", age: "28", profession: "shopkeeper"},
+      {id: 31, name: "rohan", age: "26", profession: "teacher"}
+  ];
+
+  let mergearray = arr.concat(userarray)
+  console.log(mergearray)
 }
+
+
+
